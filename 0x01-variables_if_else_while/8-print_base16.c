ca2259f7443f48621cc27655ba3725550371c9ea
ANNRACHEL1 <ann.waithira2000@gmail.com>
2023-09-08 18:24:00 -0700

    Print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+int 2;
+char i;
+for (2 = 0; 2 < 10; 2++)
+putchar((2 % 10) + '0');
+
+for (i = 'a'; i < = 'f'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+
+}
+
