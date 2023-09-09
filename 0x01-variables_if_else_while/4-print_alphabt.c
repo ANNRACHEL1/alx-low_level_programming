933d7131caa796b44478ab9c13d71106a9a18672
ANNRACHEL1 <ann.waithira2000@gmail.com>
2023-09-09 03:21:10 -0700

    alphabet in lowe case

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the alphabet in lowercase and upper case
- * Return: Always 0.
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-char i;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-for (i = 'a'; i <= 'z'; i++)
-{
-if (i != 'e'; &&i != 'q')
-putchar(i);
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
-putchar('\n');
 
-return (0);
-
-}
