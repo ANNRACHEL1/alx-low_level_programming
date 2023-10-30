03a8697f13e03afc2c262f0eb7d97326328aedf9
ANNRACHEL1 <ann.waithira2000@gmail.com>
2023-10-30 10:38:57 -0700

    0x15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 /**
  * read_textfile - reads a textfile and prints the letters
  * @filename: filename.
